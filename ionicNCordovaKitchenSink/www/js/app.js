a8e43a6883d312e946616a5944144b8326705410
Loiane Groner <me@loiane.com>
2015-03-23 17:47:07 -0300

    added some ng-cordova examples

--- ionicNCordovaKitchenSink/www/js/app.js
+++ ionicNCordovaKitchenSink/www/js/app.js
@@ -6,66 +6,129 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: 'AppCtrl'
+            })
+
+            .state('app.single', {
+                url: "/playlists/:playlistId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/playlist.html",
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            })
+
+            .state('app.start', {
+                url: "/start",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/start.html"
+                    }
+                }
+            })
+
+            .state('app.device', {
+                url: "/device",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/device.html",
+                        controller: 'DeviceCtrl'
+                    }
+                }
+            })
+
+            .state('app.network', {
+                url: "/network",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/network.html",
+                        controller: 'NetworkCtrl'
+                    }
+                }
+            })
+
+            .state('app.dialogs', {
+                url: "/dialogs",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/dialogs.html",
+                        controller: 'DialogsCtrl'
+                    }
+                }
+            })
+
+            .state('app.geolocation', {
+                url: "/geolocation",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/geolocation.html",
+                        controller: 'GeolocationCtrl'
+                    }
+                }
+            })
+
+            .state('app.batteryStatus', {
+                url: "/batteryStatus",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/batteryStatus.html",
+                        controller: 'BatteryStatusCtrl'
+                    }
+                }
+            })
+
+            .state('app.contacts', {
+                url: "/contacts",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/contacts.html",
+                        controller: 'ContactsCtrl'
+                    }
+                }
+            })
+
+            .state('app.deviceMotion', {
+                url: "/deviceMotion",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/deviceMotion.html",
+                        controller: 'DeviceMotionCtrl'
+                    }
+                }
+            })
+
+            .state('app.deviceOrientation', {
+                url: "/deviceOrientation",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/deviceOrientation.html",
+                        controller: 'DeviceOrientationCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/start');
+    });
--- ionicNCordovaKitchenSink/www/js/controllers.js
+++ ionicNCordovaKitchenSink/www/js/controllers.js
@@ -1,48 +1,275 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCordova'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+        // Form data for the login modal
+        $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    .controller('PlaylistsCtrl', function ($scope) {
+        $scope.playlists = [
+            {title: 'Reggae', id: 1},
+            {title: 'Chill', id: 2},
+            {title: 'Dubstep', id: 3},
+            {title: 'Indie', id: 4},
+            {title: 'Rap', id: 5},
+            {title: 'Cowbell', id: 6}
+        ];
+    })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+    .controller('PlaylistCtrl', function ($scope, $stateParams) {
+    })
+
+    .controller('DeviceCtrl', function ($scope, $cordovaDevice) {
+
+        var init = function () {
+            console.log("initializing device");
+            try {
+                document.addEventListener("deviceready", function () {
+                    $scope.available = $cordovaDevice.getDevice().available;
+                    $scope.cordova = $cordovaDevice.getCordova();
+                    $scope.model = $cordovaDevice.getModel();
+                    $scope.platform = $cordovaDevice.getPlatform();
+                    $scope.uuid = $cordovaDevice.getUUID();
+                    $scope.version = $cordovaDevice.getVersion();
+                }, false);
+            }
+            catch (err) {
+                console.log("Error " + err.message);
+                alert("error " + err.$$failure.message);
+            }
+        };
+
+        init();
+    })
+
+    .controller('NetworkCtrl', function ($scope, $cordovaNetwork) {
+
+        var init = function () {
+            $scope.networkType = null;
+            $scope.connectionType = null;
+
+            document.addEventListener("deviceready", function () {
+                $scope.networkType = $cordovaNetwork.getNetwork();
+
+                if ($cordovaNetwork.isOnline()) {
+                    $scope.connectionType = 'Online';
+                }
+                else if ($cordovaNetwork.isOffline()) {
+                    $scope.connectionType = 'Offline';
+                }
+                else {
+                    $scope.errorMsg = 'Error getting isOffline / isOnline methods';
+                }
+            }, false);
+        };
+
+        init();
+
+        $scope.refresh = function () {
+            console.log('Network refresh');
+            init();
+        };
+    })
+
+    .controller('DialogsCtrl', function ($scope, $cordovaDialogs) {
+
+        $scope.action = "Press any button";
+
+        $scope.alert = function () {
+            $scope.action = "Alert";
+            $cordovaDialogs.alert('Wow!');
+        };
+
+        $scope.confirm = function () {
+            $scope.action = "Confirm";
+            $cordovaDialogs.confirm('Are you sure?', "Custom title").then(function (buttonIndex) {
+                $cordovaDialogs.alert("Button index : " + buttonIndex);
+            });
+        };
+
+        $scope.prompt = function () {
+            $scope.action = "Prompt";
+            $cordovaDialogs.prompt('Please Login', "Custom title").then(function (result) {
+                $cordovaDialogs.alert("Input: " + result.input1 + "\n Button index : " + result.buttonIndex);
+            });
+        };
+
+        $scope.beep = function () {
+            $scope.action = "Beep";
+            $cordovaDialogs.beep(3);
+        };
+    })
+
+    .controller('GeolocationCtrl', function ($scope, $cordovaGeolocation) {
+
+        $scope.getLocation = function () {
+            $cordovaGeolocation
+                .getCurrentPosition({timeout: 10000, enableHighAccuracy: false})
+                .then(function (position) {
+                    console.log("position found");
+                    $scope.position = position;
+                    // long = position.coords.longitude
+                    // lat = position.coords.latitude
+                }, function (err) {
+                    console.log("unable to find location");
+                    $scope.errorMsg = "Error : " + err.message;
+                });
+        };
+
+    })
+
+    .controller('BatteryStatusCtrl', function ($scope, $timeout, $cordovaBatteryStatus) {
+
+        console.log("battery status init");
+
+        document.addEventListener("deviceready", function () {
+
+            $scope.watch = function () {
+                console.log("watching battery");
+
+                $cordovaBatteryStatus.$on('batterystatus', function (result, info) {
+                    $timeout(function () {
+                        $scope.batteryLevel = info.level;       // (0 - 100)
+                        $scope.isPluggedIn = info.isPlugged;   // bool
+                    });
+                    console.log("Info " + info.level + " " + info.isPlugged);
+                    alert("Info " + info.level + " " + info.isPlugged);
+                });
+            };
+        }, false);
+    })
+
+    .controller('ContactsCtrl', function ($scope, $log, $cordovaContacts) {
+
+        $scope.pickContact = function () {
+            document.addEventListener("deviceready", function () {
+                $cordovaContacts.pickContact().then(function (result) {
+                    console.log(JSON.stringify(result));
+                    $scope.selectedContact = result;
+                })
+            }, false);
+        };
+
+        $scope.saveContact = function (contact) {
+            document.addEventListener("deviceready", function () {
+                $cordovaContacts.save(contact).then(function (result) {
+                    console.log(JSON.stringify(result));
+                })
+            }, false);
+        };
+
+
+        $scope.removeContact = function (contact) {
+            document.addEventListener("deviceready", function () {
+                $cordovaContacts.remove(contact).then(function (result) {
+                    console.log(JSON.stringify(result));
+                })
+            }, false);
+        };
+
+    })
+
+    .controller('DeviceMotionCtrl', function ($scope, $cordovaDeviceMotion) {
+        var watchID;
+
+        $cordovaDeviceMotion
+            .getCurrentAcceleration()
+            .then(function (motion) {
+                $scope.motion = motion;
+                console.log(motion);
+            }, function (err) {
+                $scope.msg = err.message;
+                console.log(err);
+            });
+
+
+        $scope.watchAcceleration = function () {
+            var options = { frequency: 3000 };  // Update every 3 seconds
+
+            $scope.this_watch = $cordovaDeviceMotion.watchAcceleration(options);
+
+            $scope.this_watch.promise.then(
+                function () {  /* unused */
+                },
+                function (err) {
+                    $scope.msg = err.message;
+                },
+                function (motion) {
+                    $scope.motion = motion;
+                });
+        };
+
+        $scope.clearWatch = function () {
+            // use watchID from watchAccelaration()
+            $cordovaDeviceMotion.clearWatch($scope.this_watch.watchId);
+        };
+    })
+
+    .controller('DeviceOrientationCtrl', function ($scope, $timeout, $cordovaDeviceOrientation) {
+
+        var options = {frequency: 1000}; // Update every 1 seconds
+
+        $scope.getHeading = function () {
+            $cordovaDeviceOrientation
+                .getCurrentHeading()
+                .then(function (position) {
+                    $scope.heading = position;
+                }, function (err) {
+                    $scope.msg = err.message;
+                });
+        };
+
+
+        $scope.watchHeading = function () {
+            $scope.this_watch = $cordovaDeviceOrientation.watchHeading(options);
+
+            $scope.this_watch.then(
+                function () {
+                    /* unused */
+                },
+                function (err) {
+                    $scope.msg = err.message;
+                },
+                function (position) {
+                    $timeout(function () {
+                        $scope.heading = position;
+                    });
+                }
+            );
+
+        };
+
+        $scope.clearWatch = function () {
+            $cordovaDeviceOrientation.clearWatch($scope.this_watch.watchID);
+        };
+
+    });
+
