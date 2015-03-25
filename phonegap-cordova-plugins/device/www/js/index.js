f778b315f3a5668b4cf6b830dc52851ad445a6f7
Loiane Groner <me@loiane.com>
2015-03-25 13:35:33 -0300

    device example

--- phonegap-cordova-plugins/device/www/js/index.js
+++ phonegap-cordova-plugins/device/www/js/index.js
@@ -33,17 +33,11 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        document.getElementById("cordova").innerHTML = device.cordova;
+        document.getElementById("model").innerHTML = device.model;
+        document.getElementById("platform").innerHTML = device.platform;
+        document.getElementById("uuid").innerHTML = device.uuid;
+        document.getElementById("version").innerHTML = device.version;
     }
 };
