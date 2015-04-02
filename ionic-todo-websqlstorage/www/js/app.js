619abafd0b56a7f734bc00e77df2eb92ef109e8f
Loiane Groner <me@loiane.com>
2015-04-02 08:15:14 -0300

    added websql example

--- ionic-todo-websqlstorage/www/js/app.js
+++ ionic-todo-websqlstorage/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('todo', ['ionic', 'todo.controllers'])
+angular.module('todo', ['ionic', 'todo.controllers','todo.services'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
@@ -16,18 +16,4 @@ angular.module('todo', ['ionic', 'todo.controllers'])
                 StatusBar.styleDefault();
             }
         });
-    })
-
-    /*.config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-
-            .state('app.tasks', {
-                url: "/app/tasks",
-               // abstract: true,
-                templateUrl: "templates/tasks.html",
-                controller: 'TasksCtrl'
-            });
-
-        // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/app/tasks');
-    });*/
+    });
